4ed52161b3b436316efa642b06c422b33a0a0627
Krystof Rainisch <krystof.rainisch@hey.com>
2023-01-01 19:15:09 +0100

    refactor

--- src/experience/objets/Slime/Slime.tsx
+++ src/experience/objets/Slime/Slime.tsx
@@ -18,6 +18,67 @@ import fragmentShaderAgents from './moveAgents.glsl'
 const WIDTH = 1024
 const AGENT_COUNT = 1024
 
+type TUniform = {
+  [uniform: string]: THREE.IUniform<any>
+}
+
+const defaultUniforms: TUniform = {
+  time: { value: 0.0 },
+  delta: { value: 0.0 },
+  count: { value: AGENT_COUNT },
+  sampleDistance: { value: 8.0 },
+  sampleSpread: { value: 1 / 6 },
+  randomness: { value: 0.1 },
+  speed: { value: 1.0 },
+  fade: { value: 0.0 },
+  fadePower: { value: 0.0 },
+  radius: { value: 1.0 },
+  redColor: { value: new THREE.Color('#ff0000') },
+  greenColor: { value: new THREE.Color('#00ff00') },
+  blueColor: { value: new THREE.Color('#0000ff') },
+  stepInterpolation: { value: 0.0 },
+}
+
+const fillValueTexture = (texture: THREE.DataTexture) => {
+  const theArray = texture.image.data
+
+  for (let k = 0, kl = theArray.length; k < kl; k += 4) {
+    const x = Math.random() > 0.5 ? 1 : 0
+    const y = Math.random() > 0.5 ? 1 : 0
+    const z = Math.random() > 0.5 ? 1 : 0
+
+    theArray[k + 0] = 0
+    theArray[k + 1] = 0
+    theArray[k + 2] = 0
+    theArray[k + 3] = 1
+  }
+}
+
+const fillAgentTexture = (texture: THREE.DataTexture) => {
+  const theArray = texture.image.data
+  let counter = 0
+
+  for (
+    let k = 0, kl = theArray.length;
+    k < kl || counter == AGENT_COUNT;
+    k += 4
+  ) {
+    // random position in the plane
+    theArray[k + 0] = Math.random() * WIDTH
+    theArray[k + 1] = Math.random() * WIDTH
+
+    // random velocity
+    const vel = new THREE.Vector2(
+      Math.random() * 2 - 1,
+      Math.random() * 2 - 1,
+    ).normalize()
+
+    theArray[k + 2] = vel.x
+    theArray[k + 3] = vel.y
+    counter++
+  }
+}
+
 export default function Slime() {
   const {
     agentSampleDistance,
@@ -110,114 +171,40 @@ export default function Slime() {
 
   const [planeTexture, setPlaneTexture] = useState<THREE.Texture | null>(null)
 
-  const [positionTexture, setPositionTexture] = useState<THREE.Texture | null>(
-    null,
-  )
-
   const [valueVariable, setValueVariable] = useState<Variable | null>(null)
 
   const [positionVariable, setPositionVariable] = useState<Variable | null>(
     null,
   )
 
-  const fillValueTexture = (texture: THREE.DataTexture) => {
-    const theArray = texture.image.data
+  const [time, setTime] = useState(0)
+  const [delay, setDelay] = useState(0)
 
-    for (let k = 0, kl = theArray.length; k < kl; k += 4) {
-      const x = Math.random() > 0.5 ? 1 : 0
-      const y = Math.random() > 0.5 ? 1 : 0
-      const z = Math.random() > 0.5 ? 1 : 0
-
-      theArray[k + 0] = 0
-      theArray[k + 1] = 0
-      theArray[k + 2] = 0
-      theArray[k + 3] = 1
-    }
-  }
-
-  const fillPositionTexture = (texture: THREE.DataTexture) => {
-    const theArray = texture.image.data
-    let counter = 0
-
-    for (
-      let k = 0, kl = theArray.length;
-      k < kl || counter == AGENT_COUNT;
-      k += 4
-    ) {
-      // random position in the plane
-      theArray[k + 0] = Math.random() * WIDTH
-      theArray[k + 1] = Math.random() * WIDTH
-
-      // random velocity
-      const vel = new THREE.Vector2(
-        Math.random() * 2 - 1,
-        Math.random() * 2 - 1,
-      ).normalize()
-      theArray[k + 2] = vel.x
-      theArray[k + 3] = vel.y
-      counter++
-    }
-  }
-
-  const [computeUniforms, setComputeUniforms] = useState<{
+  const [stateUniforms, setStateUniforms] = useState<{
     [uniform: string]: THREE.IUniform<any>
-  }>({
-    time: { value: 0.0 },
-    delta: { value: 0.0 },
-    click: { value: new THREE.Vector2(0, 0) },
-    isClicked: { value: false },
-    fade: { value: 0.0 },
-    redColor: { value: new THREE.Color('#ff0000') },
-    greenColor: { value: new THREE.Color('#00ff00') },
-    blueColor: { value: new THREE.Color('#00ff00') },
-    stepInterpolation: { value: 0.0 },
-    fadePower: { value: 0.0 },
-    radius: { value: 1.0 },
-  })
-
-  const [agentUniforms, setAgentUniforms] = useState<{
-    [uniform: string]: THREE.IUniform<any>
-  }>({
-    time: { value: 0.0 },
-    delta: { value: 0.0 },
-    sapleDistance: { value: 0.0 },
-    sampleSpread: { value: 0.0 },
-    randomness: { value: 0.0 },
-    speed: { value: 0.0 },
-    fade: { value: 0.0 },
-    redColor: { value: new THREE.Color('#ff0000') },
-    greenColor: { value: new THREE.Color('#00ff00') },
-    blueColor: { value: new THREE.Color('#00ff00') },
-    stepInterpolation: { value: 0.0 },
-    fadePower: { value: 0.0 },
-    radius: { value: 1.0 },
-  })
+  }>(defaultUniforms)
 
   const initGpuCompute = (gl: THREE.WebGLRenderer) => {
     const gpuCompute = new GPUComputationRenderer(WIDTH, WIDTH, gl)
+    const dataType = gl.capabilities.isWebGL2 ? undefined : THREE.HalfFloatType
 
-    if (gl.capabilities.isWebGL2 === false) {
-      gpuCompute.setDataType(THREE.HalfFloatType)
-    }
-
-    const dtValue = gpuCompute.createTexture()
-    fillValueTexture(dtValue)
+    const valueTexture = gpuCompute.createTexture()
+    fillValueTexture(valueTexture)
 
     const valueVariable = gpuCompute.addVariable(
       'textureValue',
       fragmentShaderValue,
-      dtValue,
+      valueTexture,
     )
-
     setValueVariable(valueVariable)
 
-    const dtPosition = gpuCompute.createTexture()
-    fillPositionTexture(dtPosition)
+    const positionTexture = gpuCompute.createTexture()
+    fillAgentTexture(positionTexture)
 
     const agentsVariable = gpuCompute.addVariable(
       'textureAgents',
       fragmentShaderAgents,
-      dtPosition,
+      positionTexture,
     )
 
     gpuCompute.setVariableDependencies(valueVariable, [
@@ -229,62 +216,11 @@ export default function Slime() {
       agentsVariable,
       valueVariable,
     ])
-
     setPositionVariable(agentsVariable)
-    valueVariable.material.uniforms.time = { value: 0.0 }
-    valueVariable.material.uniforms.delta = { value: 0.0 }
-    valueVariable.material.uniforms.click = { value: new THREE.Vector2(0, 0) }
-    valueVariable.material.uniforms.isClicked = { value: false }
-    valueVariable.material.uniforms.count = { value: AGENT_COUNT }
-    valueVariable.material.uniforms.fade = { value: 0.0 }
-    valueVariable.material.uniforms.fadePower = { value: 0.0 }
-    valueVariable.material.uniforms.radius = { value: 1.0 }
 
-    valueVariable.material.uniforms.stepInterpolation = {
-      value: 0.0,
-    }
-
-    valueVariable.material.uniforms.redColor = {
-      value: new THREE.Color('#ff0000'),
-    }
-
-    valueVariable.material.uniforms.greenColor = {
-      value: new THREE.Color('#00ff00'),
-    }
-
-    valueVariable.material.uniforms.blueColor = {
-      value: new THREE.Color('#0000ff'),
-    }
-
-    agentsVariable.material.uniforms.time = { value: 0.0 }
-    agentsVariable.material.uniforms.delta = { value: 0.0 }
-    agentsVariable.material.uniforms.count = { value: AGENT_COUNT }
-    agentsVariable.material.uniforms.sampleDistance = { value: 8.0 }
-    agentsVariable.material.uniforms.sampleSpread = { value: 1 / 6 }
-    agentsVariable.material.uniforms.randomness = { value: 0.1 }
-    agentsVariable.material.uniforms.speed = { value: 1.0 }
-    agentsVariable.material.uniforms.fade = { value: 0.0 }
-    agentsVariable.material.uniforms.fadePower = { value: 0.0 }
-    agentsVariable.material.uniforms.radius = { value: 1.0 }
-
-    agentsVariable.material.uniforms.redColor = {
-      value: new THREE.Color('#ff0000'),
-    }
-
-    agentsVariable.material.uniforms.greenColor = {
-      value: new THREE.Color('#00ff00'),
-    }
-
-    agentsVariable.material.uniforms.blueColor = {
-      value: new THREE.Color('#0000ff'),
-    }
-
-    agentsVariable.material.uniforms.stepInterpolation = {
-      value: 0.0,
-    }
-
-    setComputeUniforms(valueVariable.material.uniforms)
-    setAgentUniforms(agentsVariable.material.uniforms)
+    Object.assign(valueVariable.material.uniforms, defaultUniforms)
+    Object.assign(agentsVariable.material.uniforms, defaultUniforms)
+    setStateUniforms(valueVariable.material.uniforms)
 
     const error = gpuCompute.init()
 
@@ -295,82 +231,48 @@ export default function Slime() {
     setComputeRenderer(gpuCompute)
   }
 
-  const [time, setTime] = useState(0)
-  const [delay, setDelay] = useState(0)
-
   useFrame((state) => {
-    //
     if (!computeRenderer) {
       initGpuCompute(state.gl)
     } else {
       const now = performance.now()
-      computeUniforms.time.value = now
-      computeUniforms.delta.value = now - time
-      computeUniforms.fade.value = fade
-      computeUniforms.fadePower.value = fadePower
-      computeUniforms.radius.value = radius
+      stateUniforms.time.value = now
+      stateUniforms.delta.value = now - time
+      stateUniforms.fade.value = fade
+      stateUniforms.fadePower.value = fadePower
+      stateUniforms.radius.value = radius
+      stateUniforms.sampleDistance.value = agentSampleDistance
+      stateUniforms.sampleSpread.value = agentSampleSpread
+      stateUniforms.randomness.value = agentRandomness
+      stateUniforms.speed.value = agentSpeed
+      stateUniforms.stepInterpolation.value = stepInterpolation
 
-      if (redColor && computeUniforms.redColor) {
-        computeUniforms.redColor.value = new THREE.Color(redColor)
-
-        agentUniforms.redColor.value = new THREE.Color(redColor)
+      if (redColor && stateUniforms.redColor) {
+        stateUniforms.redColor.value = new THREE.Color(redColor)
       }
 
-      if (greenColor) {
-        computeUniforms.greenColor.value = new THREE.Color(greenColor)
-
-        agentUniforms.greenColor.value = new THREE.Color(greenColor)
+      if (greenColor && stateUniforms.greenColor) {
+        stateUniforms.greenColor.value = new THREE.Color(greenColor)
       }
 
-      if (blueColor) {
-        computeUniforms.blueColor.value = new THREE.Color(blueColor)
-
-        agentUniforms.blueColor.value = new THREE.Color(blueColor)
+      if (blueColor && stateUniforms.blueColor) {
+        stateUniforms.blueColor.value = new THREE.Color(blueColor)
       }
-
-      agentUniforms.time.value = now
-      agentUniforms.delta.value = now - time
-      agentUniforms.sampleDistance.value = agentSampleDistance
-      agentUniforms.sampleSpread.value = agentSampleSpread
-      agentUniforms.randomness.value = agentRandomness
-      agentUniforms.speed.value = agentSpeed
-      agentUniforms.fade.value = fade
-      agentUniforms.stepInterpolation.value = stepInterpolation
-      agentUniforms.fadePower.value = fadePower
-      agentUniforms.radius.value = radius
-
       setTime(now)
-
       setDelay(delay - 1)
-
       computeRenderer.compute()
-      computeUniforms.isClicked.value = false
-      computeUniforms.click.value = new THREE.Vector2(0, 0)
-      // if (delay < 0) {
-      //   setDelay(frameDelay)
-      // }
 
       if (valueVariable) {
         setPlaneTexture(
           computeRenderer.getCurrentRenderTarget(valueVariable).texture,
         )
       }
-
-      if (positionVariable) {
-        setPositionTexture(
-          computeRenderer.getCurrentRenderTarget(positionVariable).texture,
-        )
-      }
     }
   })
 
   const handleClick = (e: ThreeEvent<MouseEvent>) => {
-    // get position of click in pixels based on WIDTH
     const x = Math.floor((e.point.x + 5) * (WIDTH / 10))
     const y = Math.floor((e.point.y + 5) * (WIDTH / 10))
-
-    computeUniforms.click.value = new THREE.Vector2(x, y)
-    computeUniforms.isClicked.value = true
   }
 
   return (
@@ -379,12 +281,6 @@ export default function Slime() {
         <mesh onClick={handleClick}>
           <planeGeometry args={[10, 10]} />
           <meshBasicMaterial map={planeTexture} />
-        </mesh>
-      )}
-      {computeRenderer && positionTexture && (
-        <mesh position-x={-20}>
-          <planeGeometry args={[10, 10]} />
-          <CustomMaterial map={positionTexture} />
         </mesh>
       )}
     </>
